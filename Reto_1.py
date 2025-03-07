e7024a9132d98d2964fde8c4adf114ab6ad6dc17
MikeBrevis <michel.brevis@gmail.com>
2025-03-06 23:47:49 -0300

    Reto N°2

--- Reto_1.py
+++ Reto_1.py
@@ -1,9 +1,10 @@
-# Reto n°1
+# Reto N°1
 
 """ Santa Claus 🎅 ha recibido una lista de números mágicos que representan regalos 🎁, pero algunos de ellos están duplicados y deben ser eliminados para evitar confusiones. Además, los regalos deben ser ordenados en orden ascendente antes de entregárselos a los elfos.
 Tu tarea es escribir una función que reciba una lista de números enteros (que pueden incluir duplicados) y devuelva una nueva lista sin duplicados, ordenada en orden ascendente. """
 
 # Define funtion, gifts_list, parameter gifts
+    
     #Var with a list, fixed_gifts_list
     
     #For x in range of len of gifts:
@@ -22,8 +23,11 @@ def gifts_list(gifts):
 
     return sorted(fixed_gifts_list)
 
-gifts_1 = gifts_list ([20, 20, 7, 6, 20, 30, 20])
+gifts_1 = gifts_list([20, 20, 7, 6, 20, 30, 20])
 print (gifts_1)
+
+gifts_2 = gifts_list([50, 20, 50, 6, 500, 30, 999, 1000, 999, 500])
+print (gifts_2)
 
 
 
